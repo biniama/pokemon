074d7ba8f92dfb4078bce7f85cd239bb368d04ec
Biniam Asnake <biniam.asnake@kaufda.de>
2020-05-11 14:04:11 +0200

    Adde Grid and initial Card

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    pokedexContainer: {
+        paddingTop: '20px',
+        paddingLeft: '50px',
+        paddingRight: '50px'
+    }
+})
 
 const Pokedex = () => {
+    const classes = useStyles();
+
+    const getPokemonCard = () => {
+        return (
+            <Grid item xs={12} sm={4}>
+                <Card>
+                    <CardContent>
+                        Hi
+                    </CardContent>
+                </Card>
+            </Grid>
+        )
+    }
+
+    // <> </> is a React Fragment
     return (
-        <div>
-            Pokedox
-        </div>
+        <>
+            <AppBar position='static'>
+                <Toolbar />
+            </AppBar>
+            <Grid container spacing={2} className={classes.pokedexContainer}>
+                {getPokemonCard()}
+            </Grid>
+        </>
     )
 }
 
